74800e377e04487e770ec021131cc576615f67ef
yooltaza-ops <yooltaza@gmail.com>
2026-06-13 15:54:41 +0100

    clean gitignore - remove sensitive and generated files

--- node_modules/docx/dist/file/drawing/inline/graphic/graphic-data/pic/blip/blip.d.ts
+++
@@ -1,3 +0,0 @@
-import { IMediaData } from '../../../../../../media';
-import { XmlComponent } from '../../../../../../xml-components';
-export declare const createBlip: (mediaData: IMediaData) => XmlComponent;
